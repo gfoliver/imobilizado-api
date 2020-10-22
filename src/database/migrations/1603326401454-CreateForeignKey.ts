3c28801cec88ac1b2471fc1632d8874f50545ba8
Eduardo Eli <55281597+elieduardo@users.noreply.github.com>
2020-10-21 23:29:29 -0300

    Add files via upload

---
+++ src/database/migrations/1603326401454-CreateForeignKey.ts
@@ -0,0 +1,19 @@
+import {MigrationInterface, QueryRunner, TableForeignKey} from "typeorm";
+
+export class CreateForeignKey1603326401454 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createForeignKey("products", new TableForeignKey({
+            name: "productArea",
+            columnNames: ["area_id"],
+            referencedColumnNames: ["id"],
+            referencedTableName: "areas",
+            onDelete: "SET NULL"
+        }));
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropForeignKey("products", "productArea");
+    }
+
+}
